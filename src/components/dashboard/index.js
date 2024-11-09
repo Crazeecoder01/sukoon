5ac7df6de32b29abdf2d0a9e2bbeb7049a3e2396
Pradneya <pradneya.prog@gmail.com>
2024-11-09 16:25:13 +0530

    Added auth and connected dashboard:

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,18 +1,14 @@
 // Dashboard
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-// import LoginButton from "./auth/login";
-// import LogoutButton from "./auth/logout";
-// import Profile from "./auth/profile";
-import { Link } from "react-router-dom";
-import Navbar from "./navbar";
+import Profile from "../auth/profile";
 
 const Dashboard = () => {
   const { isAuthenticated } = useAuth0();
 
   return (
     <div className="flex min-h-screen">
-      <aside className="bg-gray-800 text-white w-64 p-4 hidden md:block">
+      <aside className="flex bg-gray-800 text-white w-64 p-4 flex-col">
         <h2 className="text-xl mb-4">Sidebar</h2>
         <ul>
           <li className="mb-2">
@@ -31,10 +27,11 @@ const Dashboard = () => {
             </a>
           </li>
         </ul>
+        <div className="h-auto w-full grow"></div>
+        {isAuthenticated && <Profile />}
       </aside>
       <div className="flex flex-col flex-1">
         <main className="flex-1 p-4 flex flex-col items-center md:flex-row md:justify-around">
-          {isAuthenticated && <Profile />}
         </main>
       </div>
     </div>
--- src/route.js
+++ src/route.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from "./App";
-import ChatGroups from "./components/chatRoom/chatGroup";
 import Home from './components';
 import Dashboard from './components/dashboard';
 const CompRoutes = () => {
